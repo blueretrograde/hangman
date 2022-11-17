1cdfc3deceebaf9d832f038a0c37deebfb00a615
Nicholas <blueretrograde@gmail.com>
2022-11-17 14:37:45 +0000

    added a class and wrote pseudocode

---
+++ Hangman/src/main/java/org/example/Hangman.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Hangman {
+
+    //take a random word from a list of words and sets it as the "secret word"
+
+    //the user inputs a letter
+
+        //if the letter is in the word, it is revealed
+
+            //display back the word with revealed letters
+
+        //if the letter is not in the word, the user loses 1 life (display how many left out of 3)
+
+        //if the user has no lives left, the game is over
+
+
+
+
+
+
+
+}
