f417eab91c3f8ebf7ca4ab78b33625c76bfa39ca
Nicholas <blueretrograde@gmail.com>
2022-11-17 15:11:08 +0000

    added a wordPool for the game

--- Hangman/src/main/java/org/example/Words.java
+++ Hangman/src/main/java/org/example/Words.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class Words {
+
+    public static final String[] wordPool = {
+            "Buenos Aires", "Yerevan", "Canberra", "Vienna", "Minsk", "Brussels", "Nicosia", "Athens", "Prague",
+            "Bratislava", "Rome", "Stockholm", "Dublin", "London", "Washington DC", "Hanoi", "Montevideo", "Moscow",
+            "Madrid", "Seoul", "Tokyo", "Amsterdam", "Mexico City", "Chisinau", "Paris", "Ottawa", "Tallinn", "Helsinki",
+            "Oslo", "Lisbon", "Manila", "Copenhagen", "Berlin", "Warsaw", "Abu Dhabi", "Panama City", "Kuala Lumpur", "New Delhi" };
+
 }
